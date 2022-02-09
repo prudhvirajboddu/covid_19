8c4b371f3b6a34e61bdd80546ccdbbb8cd11c1fb
prudhvirajboddu <prudhvirajboddu@gmail.com>
2022-02-09 22:02:45 +0530

    html file needs to be modified

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-# import sqlite3
+from crypt import methods
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy,sqlalchemy
+from sqlalchemy import func,extract
 
 db_name='covid.db'
 
@@ -49,16 +50,19 @@ class Covid(db.Model):
 #     # print(data_dict)
 #     return jsonify({'covid': jsonData_list})
 
-
 @app.route('/')
-def covid():
+def welcome():
+    return "Welcome to HomePage. Query Results based on Country name and MMYYYY"
+
+@app.route('/covid/<country>/<ym>',methods=["GET"])
+def covid(country,ym):
     try:
-        covid=Covid.query.filter_by().all()
-        c_text='<ul>'
-        for virus in covid:
-            c_text+='<li>' + str(virus.State) + ',' + str(virus.Country) + '</li>'
-        c_text+='</ul>'
-        return c_text
+        covid=Covid.query.filter(func.lower(Covid.Country)==str.lower(country) and Covid.ObservationDate[0:2]+Covid.ObservationDate[-4:]==ym).all()
+        # c_text='<ul>'
+        # for virus in covid:
+        #     c_text+='<li>' + str(virus.State) + ',' + str(virus.Country) + '</li>'
+        # c_text+='</ul>'
+        return render_template('index.html',covid=covid)
     except Exception as e:
         error_text = "<p>The error:<br>" + str(e) + "</p>"
         hed = '<h1>Something is broken.</h1>'
